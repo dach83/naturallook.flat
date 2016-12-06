f6e76f9c79aec8f9473bd176411f9ca1db5adac8
dach83 <dach83@mail.ru>
2016-12-07 00:38:43 +0500

    123

--- js/studio.js
+++ js/studio.js
@@ -1,6 +1,24 @@
 $(function(){
 
-	$(".magicwall.desktop").magicWall({
+	// Меняем высоту magicwall при изменении ширины
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+	function ResizeMagicwall () {
+
+		var mw = $('.magicwall');
+		mw.each (function (){
+			var $this = $(this);
+			$this.innerHeight (3*$this.innerWidth()/4);
+		});
+
+	};
+
+	$(window).resize(ResizeMagicwall);
+	ResizeMagicwall ();
+
+
+	// Запуск magicwall
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+	$(".desktop .magicwall").magicWall({
 		columnsCount: '5',
 		rowsCount: '5',
 		//paused: 'true',
@@ -9,13 +27,14 @@ $(function(){
 		fixedClass: 'fixed'
 	});
 
-	$(".magicwall.tablet").magicWall({
-		columnsCount: '2',
-		rowsCount: '7',
+	$(".tablet .magicwall").magicWall({
+		columnsCount: '3',
+		rowsCount: '3',
 		//paused: 'true',
 		//loadingMode: 'chain',
 		//preloadBeforeSwitch: 'true',
 		fixedClass: 'fixed'
 	});
+
 
 });
