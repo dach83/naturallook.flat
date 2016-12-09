ed026aedee38094175f63fccdaef68b549ffe24e
dach83 <dach83@mail.ru>
2016-12-09 17:03:57 +0300

    123

--- js/portfolio.js
+++ js/portfolio.js
@@ -7,32 +7,80 @@ $(function(){
 		var mw = $('.magicwall');
 		mw.each (function (){
 			var $this = $(this);
-			$this.innerHeight (7*3*$this.innerWidth()/4);
+
+			var availWidth  = $this.innerWidth();
+			var availHeight = $this.parent().height() * 0.9;
+
+			var colsCount = 2;
+			if (availWidth < 200) {
+				colsCount = 1;
+			}
+				
+
+			var imgWidth  = availWidth / colsCount;
+			var imgHeight = 3/4 * imgWidth;
+
+			var rowsCount = Math.floor (availHeight / imgHeight);
+
+			console.log ('   availH=' + availHeight + 
+				           '   availW=' + availWidth  + 
+				           '   imgW='   + imgWidth    + 
+				           '   imgH='   + imgHeight   + 
+				           '  rowsCount=' + rowsCount + 
+				           '  colsCount=' + colsCount);
+
+			$this.magicWall('option', 'rowsCount', rowsCount);
+			$this.magicWall('option', 'columnsCount', colsCount);
+			$this.innerHeight (rowsCount*imgHeight);			
+
+			$this.magicWall('update');
+
+			//$this.innerHeight (7/2 * 3*$this.innerWidth()/4);
 			//$this.innerWidth (4*$this.innerHeight()/3);	
 		});
 
 	};
 
 	$(window).resize(ResizeMagicwall);
-	ResizeMagicwall ();
+	//ResizeMagicwall ();
 
 
-	// Запуск magicwall
+	// Инициализация magicwall
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 	$(".magicwall").magicWall({
-		columnsCount: '1',
-		rowsCount: '7',
-		//paused: 'true',
-		//loadingMode: 'chain',
-		//preloadBeforeSwitch: 'true',
-		//pauseOnHover: 'item',
-		delay: '200',
-		duration: '2000',
+		//columnsCount: '2',
+		//rowsCount: '7',
+		paused: 'true',
+		loadingMode: 'chain',
+		preloadBeforeSwitch: 'true',
+		pauseOnHover: 'item',
+		//delay: '500',
+		//duration: '800',
 		thumbSizing: '95%',
-		animations: 'slideColumn',
+		animations: 'slideColumn,slideRow,-slideColumn,-slideRow',
 		fixedClass: 'fixed'
 	});
 
-	
+	ResizeMagicwall ();
 
-});
+	
+	// Запуск magicwall при наведении мышки
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+
+	$('section').hoverDelay({
+		delayIn:  100,
+		delayOut: 100,
+		
+		handlerIn: function($element){
+			$('.magicwall').magicWall('stop');
+			$element.find('.magicwall').magicWall('start');
+		},
+
+		handlerOut: function($element){
+			$('.magicwall').magicWall('stop');
+		}
+
+	}); 
+
+
+}); // ready
